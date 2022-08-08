35a603e1073171cf2ff074a5444ac731d94f2753
maxkpow <maxkpow@gmail.com>
2022-08-08 18:10:44 +0200

    capture http requests

--- src/requests_analyzer.py
+++ src/requests_analyzer.py
@@ -1,14 +1,42 @@
 from seleniumwire import webdriver
+import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
+import gzip
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+def decode_gzip_content(content):
+    content = gzip.decompress(content).decode("utf-8")
+    return content
 
-driver.get('https://www.google.com')
+def capture_http_requests(driver):
+    for request in driver.requests:
+        if request.response:
+            content_encoding = request.response.headers['content-encoding']
+            response_body = decode_gzip_content(request.response.body) if content_encoding == "gzip" else request.response.body 
 
-for request in driver.requests:
-    if request.response:
-        print(
-            request.url,
-            request.response.status_code,
-            request.response.headers['Content-Type']
-        )
+            captured_request = {
+                "host": request.host,
+                "url": request.url, 
+                "path": request.path,
+                "method": request.method,
+                "headers": [f"{key}: {value}" for key, value in request.headers.items()],
+                "response_status": request.response.status_code,
+                "response_content_encoding": content_encoding,
+                "response_content_type": request.response.headers['content-type'],
+                "response_body": response_body,
+            }
+
+            yield captured_request
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+driver.get('https://avoska.ru/shops/')
+
+captured_requests = list(capture_http_requests(driver))
+
+df = pd.DataFrame(captured_requests)
+df.head()
+
+df.to_excel("captured_requests.xlsx")
